52369eb6982d68fe4742d12f89afa393f7b09210
dillchalisas <dillynvermeulen@gmail.com>
2025-04-10 14:19:59 +0700

    Merge remote-tracking branch 'origin/main'

--- assets/js/script.js
+++
@@ -1,7 +0,0 @@
-// Menu toggle for mobile
-document.querySelector('.menu-toggle').addEventListener('click', () => {
-    const nav = document.querySelector('nav ul');
-    nav.classList.toggle('active');
-});
-
-
